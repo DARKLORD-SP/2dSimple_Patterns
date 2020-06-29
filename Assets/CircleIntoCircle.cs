9df8ba63c5901c4d8196cc66d92a4584c5248fde
Sahil Panchal <61861503+DARKLORD-SP@users.noreply.github.com>
2020-06-29 22:33:17 +0530

    Initial Commit

---
+++ Assets/CircleIntoCircle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Trignometry.Circle
+{
+
+    /// <summary>
+    /// CircleIntoCircle Description
+    /// </summary>
+    public class CircleIntoCircle : MonoBehaviour
+    {
+        [Range(1.0f, 20.0f)]
+        public float radius = 10;
+        [Range(2, 100)]
+        public int numberOfCirclesToPlot = 10;
+        [Range(1, 10)]
+        public int iteration = 2;
+
+
+        public void OnDrawGizmos()
+        {
+            //Drawing Main Cirlce
+            Vector3 mainCirclePos = Vector3.zero;
+            Handles.color = Color.red;
+            Handles.DrawWireDisc(mainCirclePos, -Vector3.forward, radius);
+
+            float theta = 360 / numberOfCirclesToPlot;
+
+            int currentIteration = 1;
+            while (currentIteration < iteration)
+            {
+                Vector3[] allCircles = new Vector3[numberOfCirclesToPlot];
+
+                for (int i = 1; i <= numberOfCirclesToPlot; i++)
+                {
+                    float nextTheta = ((i * theta) * Mathf.PI) / 180.0f;
+
+                    Vector3 tempCircle = CreateCirlce(mainCirclePos, radius, nextTheta);
+
+                    allCircles[i - 1] = tempCircle;
+
+                    Handles.color = Color.blue;
+                    Handles.DrawWireDisc(tempCircle, Vector3.forward, radius * (iteration * 0.1f));
+                }
+
+                for (int i = 0; i < allCircles.Length; i++) 
+                {
+                    for(int j = 1; j <= numberOfCirclesToPlot; j++)
+                    {
+                        float nextTheta = ((j * theta) * Mathf.PI) / 180.0f;
+
+                        Vector3 tempCircle = CreateCirlce(allCircles[i], radius * (iteration * 0.1f), nextTheta);
+
+                        Handles.color = Color.green;
+                        Handles.DrawWireDisc(tempCircle, -Vector3.forward, radius * (iteration * 0.01f));
+                    }
+                }
+
+                currentIteration += 1;
+            }
+        }
+
+        Vector3 CreateCirlce(Vector3 referencePos, float _radius, float angleInRadians)
+        {
+            return new Vector3(referencePos.x + (_radius * Mathf.Cos(angleInRadians)),
+                    referencePos.y + (_radius * Mathf.Sin(angleInRadians)), 0);
+        }
+    }
+
+}
---
+++ Assets/CircleRecursive.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.XR;
+
+namespace Trignometry.Circle
+{
+
+    /// <summary>
+    /// CircleRecursive Description
+    /// </summary>
+    public class CircleRecursive : MonoBehaviour    
+    {
+
+        [Range(1f, 20f)]
+        public float radius = 10f;
+
+        [Range(2, 20)]
+        public int numberOfCircles = 5;
+
+        [Range(2, 100)]
+        public int iteration = 2;
+
+        private int currentIteration = 1;
+
+        public void OnDrawGizmos()
+        {
+
+            Vector3 mainCircle = Vector3.zero;
+            Handles.color = Color.red;
+            Handles.DrawWireDisc(mainCircle, -Vector3.forward, radius);
+
+            for (int i = 1; i <= numberOfCircles; i++) 
+            {
+                Vector3 iteratedCircle = mainCircle;
+                Vector3 previouslyIteratedCircle = iteratedCircle;
+                float theta = (i * (360 / numberOfCircles) * Mathf.PI) / 180.0f;
+                float _radius = 0;
+                do
+                {
+                    _radius = (iteratedCircle != mainCircle) ? _radius * ((currentIteration) * 0.1f) : radius;
+                    
+                    previouslyIteratedCircle = iteratedCircle;
+                    iteratedCircle = CreateCircle(iteratedCircle, theta, _radius);
+
+                    Handles.color = Color.Lerp(Color.blue, Color.red, Time.deltaTime / 0.2f);
+                    Handles.DrawWireDisc(iteratedCircle, -Vector3.forward, _radius );
+
+                    currentIteration++;
+                }
+                while (currentIteration != iteration);
+
+                for (int j = 1; j < numberOfCircles ; j++) {
+                    iteratedCircle = CreateCircle(previouslyIteratedCircle, theta, _radius);
+
+                    Handles.color = Color.Lerp(Color.blue, Color.yellow, Time.deltaTime / 0.2f);
+                    Handles.DrawWireDisc(iteratedCircle, -Vector3.forward, _radius * (( currentIteration) * 0.1f));
+                }
+
+
+                currentIteration = 1;
+            }
+
+        }
+
+        Vector3 CreateCircle(Vector3 referenceCircle, float angleInRaidians, float radius) 
+        {
+            return new Vector3(referenceCircle.x + (radius * Mathf.Cos(angleInRaidians)), referenceCircle.y + (radius * Mathf.Sin(angleInRaidians)), 0);
+        }
+
+    }
+
+}
